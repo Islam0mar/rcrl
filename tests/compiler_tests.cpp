aeb99f5e89053e9a84d11c21aa7bd5364626270d
islam omar ahmed <io1131@fayoum.edu.eg>
2021-06-27 02:44:35 +0200

    fix compiler tests

--- tests/compiler_tests.cpp
+++ tests/compiler_tests.cpp
@@ -1,20 +1,23 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../src/rcrl/rcrl.h"
 #include "doctest/doctest/doctest.h"
 
-#include "../src/rcrl/rcrl.h"
-
 TEST_CASE("single variables") {
-	int exitcode = 0;
+  int exitcode = 0;
 
-	rcrl::submit_code("int a = 5;", rcrl::VARS);
-	while(!rcrl::try_get_exit_status_from_compile(exitcode));
-	REQUIRE_FALSE(exitcode);
-	rcrl::copy_and_load_new_plugin();
+  rcrl::Plugin p;
 
-	rcrl::submit_code("a++;", rcrl::ONCE);
-	while(!rcrl::try_get_exit_status_from_compile(exitcode));
-	REQUIRE_FALSE(exitcode);
-	rcrl::copy_and_load_new_plugin();
+  p.CompileCode("int a = 5;");
+  while (!p.TryGetExitStatusFromCompile(exitcode))
+    ;
+  REQUIRE_FALSE(exitcode);
+  p.CopyAndLoadNewPlugin();
+
+  p.CompileCode("a++;");
+  while (!p.TryGetExitStatusFromCompile(exitcode))
+    ;
+  REQUIRE_FALSE(exitcode);
+  p.CopyAndLoadNewPlugin();
 }
 
 #ifndef __APPLE__
@@ -26,13 +29,16 @@ TEST_CASE("single variables") {
 #endif
 
 std::vector<int> g_pushed_ints;
-RCRL_SYMBOL_EXPORT void test_ctor_dtor_order(int num) { g_pushed_ints.push_back(num); }
+RCRL_SYMBOL_EXPORT void test_ctor_dtor_order(int num) {
+  g_pushed_ints.push_back(num);
+}
 
 TEST_CASE("destructor order") {
-	int exitcode = 0;
+  int exitcode = 0;
 
-	// about to check destructor order
-    rcrl::submit_code(R"raw(
+  // about to check destructor order
+  rcrl::Plugin p;
+  p.CompileCode(R"raw(
 //vars
 int num_instances = 0;
 
@@ -48,20 +54,19 @@ struct S {
 S a1;
 S a2;
 )raw");
-    while(!rcrl::try_get_exit_status_from_compile(exitcode));
-	REQUIRE_FALSE(exitcode);
+  while (!p.TryGetExitStatusFromCompile(exitcode))
+    ;
+  REQUIRE_FALSE(exitcode);
 
-	rcrl::copy_and_load_new_plugin();
+  p.CopyAndLoadNewPlugin();
 
-	REQUIRE(g_pushed_ints.size() == 2);
-	REQUIRE(g_pushed_ints[0] == 1);
-	REQUIRE(g_pushed_ints[1] == 2);
+  REQUIRE(g_pushed_ints.size() == 2);
+  REQUIRE(g_pushed_ints[0] == 1);
+  REQUIRE(g_pushed_ints[1] == 2);
 
-	rcrl::cleanup_plugins();
-
-	REQUIRE(g_pushed_ints.size() == 4);
-	REQUIRE(g_pushed_ints[2] == 2);
-	REQUIRE(g_pushed_ints[3] == 1);
+  REQUIRE(g_pushed_ints.size() == 4);
+  REQUIRE(g_pushed_ints[2] == 2);
+  REQUIRE(g_pushed_ints[3] == 1);
 }
 
 #endif
